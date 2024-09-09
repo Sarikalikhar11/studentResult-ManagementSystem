d9f46ca0dfe14ffdbe947492b5d5d3d40901f623
komal-khatik <komalkhatik77@gmail.com>
2024-09-08 20:45:04 -0700

    changes done

--- backend/router/studentRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { loginStudent, registerStudent } from "../controller/studentController.js";
-
-const studentRouter = express.Router();
-
-studentRouter.post("/register", registerStudent);
-studentRouter.post("/login", loginStudent);
-
-export default studentRouter;
